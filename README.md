217563216d16c18b30dc32a94971a9072c74bd6e
James Kienle <james.kienle@gmail.com>
2024-12-01 15:05:07 -0800

    Setup Application interfaces (#2)

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ The Manatee Game Engine
 
 ## Getting Started
 
-If this is your first time using Manatee, we highly recommend you complete the following steps to
+If this is your first time using Manatee, I highly recommend you complete the following steps to
 get started:
 
 1. Install the latest version of VS Code. Manatee only has official support for VS Code at this
-   time, however we plan on adding support for other editors once the engine is a little closer to
+   time, however I plan on adding support for other editors once the engine is a little closer to
    a completed stage.
 2. Install all of the recommended VS Code extensions found in `.vscode/extensions.json`
-3. Install the latest nightly version of Zig and ZLS. We recommend installation via the VS Code Zig
+3. Install the latest nightly version of Zig and ZLS. I recommend installation via the VS Code Zig
    extension commands.
 4. Install the Vulkan SDK. Support for DirectX 12, OpenGL, and Metal is planned for future releases
    as Manatee becomes more stable and feature complete, but as of right now Vulkan is the only
@@ -20,7 +20,7 @@ get started:
 ## A Note About Zig Versioning
 
 As of right now, Manatee runs on the latest, nightly release of Zig. Once Zig hits version 1.0.0,
-we'll likely change this requirement to the most current, stable release of Zig. Using nightly
+I'll likely change this requirement to the most current, stable release of Zig. Using nightly
 ensures Manatee stays up to date with the most current Zig feature set and syntax, ensuring
 stability around future releases.
 
--- src/game.zig
+++ src/game.zig
@@ -1,21 +1,47 @@
-const std = @import("std");
+const system = @import("system.zig");
 
-/// The main Game struct. This struct manages your game's configuration, event loop, and contains
-/// all of the logic required to create a game!
+pub const game_config = @import("game/game_config.zig");
+
+/// The Manatee Game.
+///
+/// Example Usage:
+/// ```zig
+/// const manatee = @import("manatee");
+///
+/// pub fn main() !void {
+///     var editor_game = manatee.Game.init(.{ .title = "My Game" });
+///     defer editor_game.deinit();
+///     try editor_game.run();
+/// }
+/// ```
+///
+/// This struct handles your game's overall configuration and scene management. Unless you're doing
+/// something truly bespoke, the `Game` struct will be instantiated and called in your game's main
+/// entrypoint. It provides reasonable defaults, including an App based off of your target OS, an
+/// allocator, and automatic GPU configuration based off of the best API for your current system.
+///
+/// For more information on configuration (and what parts of this struct can be overridden via
+/// configuration), see the `GameConfig` struct.
 pub const Game = struct {
-    const Self = @This();
+    app: system.app.App,
+    title: []const u8,
 
-    pub fn init() Self {
-        return .{};
+    pub fn init(config: game_config.GameConfig) Game {
+        const app = config.app orelse system.app.getApp();
+        return .{
+            .app = app,
+            .title = config.title,
+        };
     }
 
     /// Starts your game's event loop and runs until terminated
-    pub fn run(self: *Self) !void {
-        _ = self;
-        std.debug.print("Running Game!", .{});
+    pub fn run(self: *Game) !void {
+        // Start the application event loop
+        self.app.run();
     }
 
-    pub fn deinit(self: *Self) void {
+    pub fn deinit(self: *Game) void {
+        self.app.deinit();
         self.* = undefined;
     }
 };
