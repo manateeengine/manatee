3d51a1f40efac922f97a949bad06388113b02f61
James Kienle <james.kienle@gmail.com>
2024-12-06 14:43:26 -0800

    Add GUIDs to Win32 bindings

--- src/bindings/win32.zig
+++ src/bindings/win32.zig
@@ -7,6 +7,7 @@ pub const foundation = @import("win32/foundation.zig");
 pub const system = @import("win32/system.zig");
 pub const ui = @import("win32/ui.zig");
 
+pub const Guid = c.zig.Guid;
 pub const l = c.zig.L;
 pub const UInt = c_uint;
 pub const WinApi = std.os.windows.WINAPI;
