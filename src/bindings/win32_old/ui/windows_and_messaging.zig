1449afe3ab2dcdf29743a9164088d9b444954aa6
James Kienle <james.kienle@gmail.com>
2024-12-30 00:43:39 -0800

    Lay the Foundations for a Win32 Bindings Rewrite

--- src/bindings/win32_old/ui/windows_and_messaging.zig
+++ src/bindings/win32_old/ui/windows_and_messaging.zig
@@ -5,6 +5,7 @@ pub const defWindowProcW = c.ui.windows_and_messaging.DefWindowProcW;
 pub const destroyWindow = c.ui.windows_and_messaging.DestroyWindow;
 pub const dispatchMessageW = c.ui.windows_and_messaging.DispatchMessageW;
 pub const getMessageW = c.ui.windows_and_messaging.GetMessageW;
+pub const getWindowLongW = c.ui.windows_and_messaging.GetWindowLongW;
 pub const postQuitMessage = c.ui.windows_and_messaging.PostQuitMessage;
 pub const registerClassExW = c.ui.windows_and_messaging.RegisterClassExW;
 pub const showWindow = c.ui.windows_and_messaging.ShowWindow;
@@ -13,6 +14,7 @@ pub const translateMessage = c.ui.windows_and_messaging.TranslateMessage;
 pub const Msg = c.ui.windows_and_messaging.MSG;
 pub const WndClassExW = c.ui.windows_and_messaging.WNDCLASSEXW;
 pub const WndClassStyles = c.ui.windows_and_messaging.WNDCLASS_STYLES;
+pub const WindowLongPtrIndex = c.ui.windows_and_messaging.WINDOW_LONG_PTR_INDEX;
 
 pub const CwUseDefault = c.ui.windows_and_messaging.CW_USEDEFAULT;
 pub const pmRemove = c.ui.windows_and_messaging.PM_REMOVE;
